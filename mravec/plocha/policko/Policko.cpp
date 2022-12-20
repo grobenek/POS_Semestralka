ff25f1576564283ab6885e3fb7f6bbac5bb48b37
Peter Szathmary <peter.szathmary01@gmail.com>
2022-12-20 09:59:59 +0100

    Policko finished

--- mravec/plocha/policko/Policko.cpp
+++ mravec/plocha/policko/Policko.cpp
@@ -3,3 +3,32 @@
 //
 
 #include "Policko.h"
+
+Policko::Policko(TypPolicka typPolicka) : typPolicka(typPolicka)
+{}
+
+TypPolicka Policko::getTypPolicka() const
+{
+    return typPolicka;
+}
+
+void Policko::setTypPolicka(TypPolicka pTypPolicka)
+{
+    Policko::typPolicka = pTypPolicka;
+}
+
+void Policko::zmenTypPolicka()
+{
+    this->typPolicka == BIELE ? this->typPolicka = CIERNE : this->typPolicka = BIELE;
+}
+
+std::string Policko::getZnak() const
+{
+    if (this->typPolicka == BIELE)
+    {
+        return "\033[47m \033[0m";
+    } else
+    {
+        return "\033[40m \033[0m";
+    }
+}
--- mravec/plocha/policko/Policko.h
+++ mravec/plocha/policko/Policko.h
@@ -5,11 +5,24 @@
 #ifndef MRAVEC_POLICKO_H
 #define MRAVEC_POLICKO_H
 
+#include <string>
+#include "../typPolicka/TypPolicka.h"
 
 class Policko
 {
+private:
+    TypPolicka typPolicka;
 
+public:
+    explicit Policko(TypPolicka typPolicka);
+
+    TypPolicka getTypPolicka() const;
+
+    void setTypPolicka(TypPolicka pTypPolicka);
+
+    void zmenTypPolicka();
+
+    std::string getZnak() const;
 };
-
 
 #endif //MRAVEC_POLICKO_H
